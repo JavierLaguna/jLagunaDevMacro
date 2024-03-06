d567560a354c47f1f817d20f3bceae9c55e850e9
Javier Laguna <javier.laguna@innocv.com>
2024-03-06 19:00:10 +0100

    Add testMacroOnANonEnum

--- Tests/jLagunaDevMacroTests/EnumTitleMacroTests.swift
+++ Tests/jLagunaDevMacroTests/EnumTitleMacroTests.swift
@@ -36,4 +36,24 @@ final class EnumTitleMacroTests: XCTestCase {
           """
       }
     }
+    
+    func testMacroOnANonEnum() {
+      assertMacro(["EnumTitle": EnumTitleMacro.self]) {
+        """
+        @EnumTitle
+        struct Foo {
+            let bar: String
+        }
+        """
+      } diagnostics: {
+          """
+          @EnumTitle
+          ┬─────────
+          ╰─ 🛑 This macro can only be applied to a enum.
+          struct Foo {
+              let bar: String
+          }
+          """
+      }
+    }
 }
